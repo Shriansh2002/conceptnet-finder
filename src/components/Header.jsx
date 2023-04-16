04b158de04ef3247b4ea117f64576e3de44e83e0
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-04-16 19:45:23 +0530

    Header Issue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ const Header = () => {
 	return (
 		<header className="fixed top-0 left-0 w-full bg-blue-500 text-white py-4 z-50">
 			<div className="container mx-auto px-4 flex justify-between items-center">
-				<h1 className="text-xl font-bold">ConceptNet Finder</h1>
+				<a href="/" className="hover:text-gray-200">
+					<h1 className="text-xl font-bold">ConceptNet Finder</h1>
+				</a>
 				<nav>
 					<ul className="flex space-x-4">
 						<li>
