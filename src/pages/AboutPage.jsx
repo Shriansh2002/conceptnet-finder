ca03b510112b1f062ef75a7808d1528476fddb28
Shriansh Agarwal <41548480+Shriansh2002@users.noreply.github.com>
2023-04-17 08:28:44 +0530

    UI Fix

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -7,7 +7,7 @@ const AboutPage = () => {
 			<Header />
 
 			<main className="flex-1">
-				<div className="container mx-auto p-4 flex-1 mt-24">
+				<div className="container mx-auto p-4 flex-1 mt-16 mb-12">
 					<h1 className="text-4xl font-bold mb-4">About</h1>
 					<div className="flex flex-col md:flex-row md:space-x-4">
 						<div className="flex-1 text-justify">
